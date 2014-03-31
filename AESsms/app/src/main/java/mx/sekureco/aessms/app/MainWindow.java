93981062995b93905d1cd6f573a96c55f5defd1f
chidmdxx <rachid@outlook.com>
2014-03-30 19:40:17 -0600

    update a 5.3

--- AESsms/app/src/main/java/mx/sekureco/aessms/app/MainWindow.java
+++ AESsms/app/src/main/java/mx/sekureco/aessms/app/MainWindow.java
@@ -18,6 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+
 public class MainWindow extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
